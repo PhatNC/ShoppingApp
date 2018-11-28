ffcf3062d25b4766dfe519ffea1da91dc18b1cbf
Hoàng Khánh <khanh.ht1996@gmail.com>
2018-11-28 15:52:03 +0700

    Fix SplashScreen can run on iOS

--- app/components/SplashScreen.js
+++ app/components/SplashScreen.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
-import { StatusBar, View, Text, ActivityIndicator, ImageBackground, StyleSheet, Dimensions } from 'react-native';
+import React, { Component } from "react";
+import {
+  StatusBar,
+  View,
+  Platform,
+  ActivityIndicator,
+  ImageBackground,
+  StyleSheet,
+  Dimensions
+} from "react-native";
 
-import BG_IMAGE from '../media/img/logo.png'
+import BG_IMAGE from "../media/img/logo.png";
 
 export default class SplashScreen extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <ImageBackground source={BG_IMAGE} style={styles.bgImage}>
-                    <StatusBar backgroundColor="#2c3e50" barStyle="light-content" />
-                    <ActivityIndicator color={'black'} size={50} />
-                </ImageBackground>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ImageBackground source={BG_IMAGE} style={styles.bgImage}>
+          <StatusBar backgroundColor="#2c3e50" barStyle="light-content" />
+          <ActivityIndicator
+            color={"black"}
+            size={Platform.OS === "ios" ? 1 : 50}
+          />
+        </ImageBackground>
+      </View>
+    );
+  }
 }
 
-const SCREEN_WIDTH = Dimensions.get('window').width;
-const SCREEN_HEIGHT = Dimensions.get('window').height;
-
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SCREEN_HEIGHT = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-    bgImage: {
-        flex: 1,
-        width: SCREEN_WIDTH,
-        height: SCREEN_HEIGHT,
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-})
+  bgImage: {
+    flex: 1,
+    width: SCREEN_WIDTH,
+    height: SCREEN_HEIGHT,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
