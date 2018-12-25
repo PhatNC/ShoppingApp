3605feb3fbed0e63fb30b558fd9c78da13f574b0
Hoàng Khánh <khanh.ht1996@gmail.com>
2018-12-25 09:13:51 +0700

    A giant of change

--- android/app/src/main/java/com/shoppingapp/MainApplication.java
+++ android/app/src/main/java/com/shoppingapp/MainApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationReactPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
+            new NavigationReactPackage(),
+            new MapsPackage(),
             new VectorIconsPackage(),
             new MapsPackage()
       );
