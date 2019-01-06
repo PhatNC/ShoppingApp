f452135b8faf28816e91b22386ca69aac319a159
DESKTOP-KG6H2L8\rrinx <1550601@gm.uit.edu.vn>
2019-01-06 11:12:02 +0700

    Refresh

--- app/components/Shop/Home/HomeView.js
+++ app/components/Shop/Home/HomeView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, ScrollView } from "react-native";
+import { View, Text, TouchableOpacity, ScrollView, RefreshControl } from "react-native";
 
 import Collection from "./Collection";
 import Category from "./Category";
@@ -8,14 +8,43 @@ import { styles } from "../../../styles/styles";
 import TopBar from "../TopBar";
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      refreshing: false
+    };
+  }
+
+  _onRefresh = () => {
+    this.setState({ refreshing: true });
+    setTimeout(() => {
+      this.setState({ refreshing: false });
+    }, 700);
+  };
+  
   render() {
     return (
       <View style={styles.wrapper}>
         <TopBar navigation={this.props.parentNavigation} />
-        <ScrollView style={styles.tabContainer}>
+        <ScrollView
+          style={styles.tabContainer}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.refreshing}
+              onRefresh={this._onRefresh}
+            />
+          }
+        >
           {/* <Collection navigation={this.props.navigation} /> */}
-          <Category navigation={this.props.navigation} />
-          <TopProduct navigation={this.props.navigation} />
+
+          {this.state.refreshing ? (
+            <View />
+          ) : (
+            <View>
+              <Category navigation={this.props.navigation} />
+              <TopProduct navigation={this.props.navigation} />
+            </View>
+          )}
         </ScrollView>
       </View>
     );
--- app/components/Shop/Home/TopProduct/index.js
+++ app/components/Shop/Home/TopProduct/index.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
-import { View, Text, Image, StyleSheet, Dimensions, ScrollView, TouchableOpacity, FlatList, ListView } from 'react-native';
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+  ListView
+} from "react-native";
 
 // import TopProductItem from './TopProductItem';
-import { styles } from '../../../../styles/styles';
+import { styles } from "../../../../styles/styles";
 
-
-const { height, width } = Dimensions.get('window');
+const { height, width } = Dimensions.get("window");
 
 export default class TopProduct extends Component {
   constructor(props) {
@@ -26,40 +35,57 @@ export default class TopProduct extends Component {
       textContainer
     } = topProductStyles;
     return (
-
       <View style={container}>
         <View style={titleContainer}>
           <Text style={styles.titleText}>Top Cakes</Text>
-        </View >
-        {(this.props.products.length) ?
+        </View>
+        {this.props.products.length ? (
           <ListView
             contentContainerStyle={body}
             enableEmptySections
-            dataSource={new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }).cloneWithRows(this.props.products)}
+            dataSource={new ListView.DataSource({
+              rowHasChanged: (r1, r2) => r1 !== r2
+            }).cloneWithRows(this.props.products)}
             renderRow={product => (
-              <TouchableOpacity style={productContainer} onPress={() => this.props.navigation.navigate('PRODUCT_DETAIL', product)}>
-                <Image source={{ uri: `${product.image[0]}` }} style={productImage} />
+              <TouchableOpacity
+                style={productContainer}
+                onPress={() =>
+                  this.props.navigation.navigate("PRODUCT_DETAIL", product)
+                }
+              >
+                <Image
+                  source={{ uri: `${product.image[0]}` }}
+                  style={productImage}
+                />
                 <View style={textContainer}>
                   <Text style={styles.contentText}>{product.name}</Text>
                   <Text style={styles.priceText}>{product.price},00$</Text>
                 </View>
-
               </TouchableOpacity>
             )}
             renderSeparator={(sectionId, rowId) => {
-              if (rowId % 2 === 1) return <View style={{ width, height: 10 }} />;
+              if (rowId % 2 === 1)
+                return <View style={{ width, height: 10 }} />;
               return null;
             }}
           />
-          : <View style={styles.wrapper}>
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-              <Text style={{ color: '#90A4AE', fontSize: 20 }}>There is no item.</Text>
+        ) : (
+          <View style={styles.wrapper}>
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text style={{ color: "#90A4AE", fontSize: 20 }}>
+                There is no item.
+              </Text>
             </View>
           </View>
-        }
-
+        )}
       </View>
-    )
+    );
   }
 }
 
@@ -68,33 +94,33 @@ const productImageHeight = (produceWidth / 361) * 452;
 
 const topProductStyles = StyleSheet.create({
   container: {
-    backgroundColor: '#FFF',
-    shadowColor: '#2E272B',
+    backgroundColor: "#FFF",
+    shadowColor: "#2E272B",
     shadowOffset: { width: 0, height: 3 },
     shadowOpacity: 0.2,
     padding: 10,
     paddingTop: 0,
     elevation: 3,
-    marginBottom: 20,
+    marginBottom: 20
   },
   titleContainer: {
     height: 40,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
   body: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    flexWrap: 'wrap',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    flexWrap: "wrap"
   },
   productContainer: {
-    width: produceWidth,
+    width: produceWidth
   },
   productImage: {
     width: produceWidth,
     height: productImageHeight
   },
   textContainer: {
-    justifyContent: 'space-between',
+    justifyContent: "space-between",
     flex: 1,
     paddingTop: 5
   }
